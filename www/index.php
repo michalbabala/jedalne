84f3d81add2aa464dbe9be7c8b4d42d3b52236c0
ondrejemilbabala <ondrejemilbabala@gmail.com>
2015-09-04 10:27:13 +0200

    connect

--- www/index.php
+++ www/index.php
@@ -3,4 +3,16 @@
 
 	print_r($_GET);
 	print_r($_POST);
+	
+require "connect.php";
+
+$query = 'SELECT *
+              FROM users';
+$result = mysql_query($query, $db) or die(mysql_error($db));
+        while ($row = mysql_fetch_array($result)) {		   
+		   $CLEN[$row["user_id"]] = $row["username"];
+		}
+		
+		echo "---".$CLEN[$row[2]]."---";
+
 ?>
