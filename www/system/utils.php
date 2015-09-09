de7dbf2ee93408f23e1e282aaa22d46aba8771d3
michalbabala <michalbabala@gmail.com>
2015-09-09 20:03:08 +0200

    random string generator

--- www/system/utils.php
+++ www/system/utils.php
@@ -12,4 +12,14 @@ function strpos_r($haystack, $needle)
     }
     return $seeks;
 }
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 ?>
